0f9f81812891980b9bff0b387c88e05fa7a09e48
mairon <maironcontatos@hotmail.com>
2014-09-20 13:45:31 -0300

    config application add .md

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Empregos
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     initializer 'setup_asset_pipeline', :group => :production  do |app|
-      
+
           # We don't want the default of everything that isn't js or css, because it pulls too many things in
           app.config.assets.precompile.shift
 
@@ -29,7 +29,7 @@ module Empregos
             File.extname(path).in? [
               '.html', '.erb', '.haml',                 # Templates
               '.png',  '.gif', '.jpg', '.jpeg',         # Images
-              '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+              '.md',   '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
             ]
           end)
         end
